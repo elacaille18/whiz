e0fc78252fb6a39c6a22aa213d3db9d505efd83a
Cyrille Ducret <cyrille.cpi@gmail.com>
2016-05-24 13:27:46 -0500

    fixing pundit on homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => :home
 
-  after_action :verify_authorized, :except => :index, unless: :devise_controller?
+  after_action :verify_authorized, :except => :home, unless: :devise_controller?
   after_action :verify_policy_scoped, :only => :index, unless: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
