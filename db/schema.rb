4128f607122b35060f0acb1c6123aa50be096e3d
Eric Lacaille <elacaille0@gmail.com>
2016-05-24 11:51:58 +0200

    add email receiver

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524092835) do
+ActiveRecord::Schema.define(version: 20160524094856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160524092835) do
     t.string   "parcel_description"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "receiver_email"
   end
 
   add_index "missions", ["trip_id"], name: "index_missions_on_trip_id", using: :btree
