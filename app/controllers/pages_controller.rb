b34a4799695ed2d710c7b53db079cdb5d518c3ef
Cyrille Ducret <cyrille.cpi@gmail.com>
2016-05-24 16:47:07 -0500

    trip CRUD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
-
   # Allow visitor to see the home page
   skip_before_action :authenticate_user!, only: :home
+  skip_after_action :verify_authorized, only: :home
 
   def home
   end
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,23 +1,58 @@
 class TripsController < ApplicationController
 
+  before_action :set_trip ,only: [:show, :edit, :update, :destroy]
+
+  def index
+    @trips = policy_scope(Trip)
+  end
+
   def new
     @trip = Trip.new
     authorize @trip
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    authorize @trip
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
+
   def edit
+
   end
 
   def update
+    @trip.update(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @trip.destroy
+
+    redirect_to trips_path
   end
 
   private
 
+  def trip_params
+    params.require(:trip).permit(:starts_at, :ends_at, :departure_city, :arrival_city)
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+    authorize @trip
+  end
+
 
 end
+
