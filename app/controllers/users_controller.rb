2b772d75654c768b92d3f688bb73365e8a199c2f
louis steenbrink <louis.steenbrink@gmail.com>
2016-05-25 18:54:41 +0200

    add cloudinary photo uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = current_user
     authorize @user
   end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :picture, :picture_cache)
+  end
 end
