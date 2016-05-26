b9110839a5c8253cc4885d4b6098788fd56b3fda
olivierjonquet <jonqueto@hotmail.fr>
2016-05-26 19:02:40 +0200

    sauvegarde test

--- db/schema.rb
+++ db/schema.rb
@@ -109,7 +109,7 @@ ActiveRecord::Schema.define(version: 20160526212652) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
-    t.boolean  "admin",                  default: false, null: false
+    t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "uid"
     t.string   "picture"
