578a4c8fe05fc9cfd940d1efd0775adcfcbcae81
Eric Lacaille <elacaille0@gmail.com>
2016-06-15 16:51:12 +0200

    connexion user to stripe connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, omniauth_providers: [:facebook, :stripe_connect]
 
 
   # Transporteur
